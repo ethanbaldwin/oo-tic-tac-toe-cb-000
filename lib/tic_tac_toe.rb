60157d88d1131b3ea67203d133b7e380dd76a3ab
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-11-26 23:53:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,12 +57,12 @@ class TicTacToe
         counter+=1
       end
     end
-    return counter
+    @turn_count = counter
+    return counter  
   end
 
   def current_player()
-    turn_count = turn_count
-    if turn_count % 2 == 0
+    if @turn_count % 2 == 0
       return "X"
     else
       return "O"
